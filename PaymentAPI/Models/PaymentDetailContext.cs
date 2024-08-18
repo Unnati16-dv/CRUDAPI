a91c8df742a0f175f040d30e0f87b423c9038ce0
Unnati16-dv <vermaunnati16@gmail.com>
2024-08-18 16:14:58 +0530

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    // In order to inherit the Db Context, we have to install nuget packages of EntityFrameworkCore and EntityFrameworkCore.SqlServer
+    public class PaymentDetailContext: DbContext
+    {
+        // Passing this option to the constructor and the base one too is a syntax that we use in order to use EF.
+        // In this options, we have to pass which db we will be using (mysql, sql etc) and also pass the connection string.
+        // This we do in program.cs file
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options) { }
+        // We need a physical table corresponding to the model that we passed to the DbSet i.e PaymentDetail
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
